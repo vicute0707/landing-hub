8a6797bd2760e38706c68e6be518df46ec3dbd03
vicute0707 <vi0978294041@gmail.com>
2025-12-18 18:20:30 +0700

    fix ipn momo

--- backend/src/services/payment/momoService.js
+++ backend/src/services/payment/momoService.js
@@ -7,7 +7,7 @@ class MomoService {
         this.accessKey = process.env.MOMO_ACCESS_KEY || '';
         this.secretKey = process.env.MOMO_SECRET_KEY || '';
         this.endpoint = process.env.MOMO_ENDPOINT || 'https://test-payment.momo.vn/v2/gateway/api/create';
-        this.redirectUrl = process.env.MOMO_REDIRECT_URL || 'https://landinghub.shop/payment/momo/callback';
+        this.redirectUrl = process.env.MOMO_REDIRECT_URL || 'https://api.landinghub.shop/api/payment/momo/callback';
         this.ipnUrl = process.env.MOMO_IPN_URL || 'https://api.landinghub.shop/api/payment/momo/ipn';
         this.requestType = 'captureWallet';
     }
